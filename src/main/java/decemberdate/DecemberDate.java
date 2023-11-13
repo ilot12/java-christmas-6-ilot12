c8add78decb567dfef9e71f9d44a301760f0db5c
ilot12 <rlaehdwn60@gmail.com>
2023-11-13 13:11:52 +0900

    Improve: validate에 예외 종류 추가
    생성자로 String을 받아 0자 입력과 수가 아닌 입력을 예외처리 하도록 추가

---
+++ src/main/java/decemberdate/DecemberDate.java
@@ -0,0 +1,42 @@
+package decemberdate;
+
+public class DecemberDate {
+    
+    private int date;
+    private boolean isWeekend;
+    
+    public DecemberDate(String dateText) {
+        int validatedDate = validateDate(dateText);
+        this.date = validatedDate;
+        this.isWeekend = calculateIsWeekend(validatedDate);
+    }
+    
+    private int validateDate(String dateText) throws IllegalArgumentException{
+        if (dateText.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+        if (!dateText.matches("[-0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+        int date = Integer.parseInt(dateText);
+        if (date <= 0) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+        return date;
+    }
+    
+    private boolean calculateIsWeekend(int date) {
+        if (date % 7 == 1 || date % 7 == 2) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int getDate() {
+        return this.date;
+    }
+    
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+}
