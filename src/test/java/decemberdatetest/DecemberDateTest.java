34a6f17ff656f01363b6437bc3d584b6ff4bcd52
ilot12 <rlaehdwn60@gmail.com>
2023-11-13 13:27:40 +0900

    test: decemberDate 예외처리 Test추가

---
+++ src/test/java/decemberdatetest/DecemberDateTest.java
@@ -0,0 +1,30 @@
+package decemberdatetest;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import decemberdate.DecemberDate;
+
+public class DecemberDateTest {
+    
+    @Test
+    void inputEmptyTest() {
+        String emptyString = "";
+        assertThatThrownBy(() -> new DecemberDate(emptyString))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void inputNotNumberTest() {
+        String notNumber = "abc";
+        assertThatThrownBy(() -> new DecemberDate(notNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void inputNegativeNumberTest() {
+        String negativeNumber = "-3";
+        assertThatThrownBy(() -> new DecemberDate(negativeNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
