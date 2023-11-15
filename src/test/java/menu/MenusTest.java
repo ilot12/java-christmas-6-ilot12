d4346dd1244e916c02e030685d40b553ba5aabf4
ilot12 <rlaehdwn60@gmail.com>
2023-11-15 12:54:38 +0900

    test: Menus클래스 테스트 작성

---
+++ src/test/java/menu/MenusTest.java
@@ -0,0 +1,60 @@
+package menu;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class MenusTest {
+    
+    @Test
+    void menusInputEmpty() {
+        assertThatThrownBy(() -> new Menus(""))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void menusWrongFormat1() {
+        assertThatThrownBy(() -> new Menus("티본스테이크-1-1"))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void menusWrongFormat2() {
+        assertThatThrownBy(() -> new Menus("티본스테이크"))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void menusNotExist() {
+        assertThatThrownBy(() -> new Menus("삼각김밥-1"))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void menusQuantityNotNumber() {
+        assertThatThrownBy(() -> new Menus("티본스테이크-a"))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void menusQuantityNotValidNumber() {
+        assertThatThrownBy(() -> new Menus("티본스테이크-0"))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void menusDuplication() {
+        assertThatThrownBy(() -> new Menus("티본스테이크-1,티본스테이크-2"))
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void menusNoException() {
+        MenusInfo menusInfo = new MenusInfo();
+        Menus menus = new Menus("티본스테이크-2,제로콜라-1");
+        assertEquals(2, menus.getMenus().get(menusInfo.getMenu("티본스테이크")));
+        assertEquals(1, menus.getMenus().get(menusInfo.getMenu("제로콜라")));
+    }
+}
