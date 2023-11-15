c51320f6e66202c1339c38bb698e91f8fca25477
ilot12 <rlaehdwn60@gmail.com>
2023-11-15 16:41:28 +0900

    fix: validate 수정
    1 ~ 31에 해당하지 않은 입력시 예외처리하는데 31초과를 예외처리 하지 않던 오류 수정

--- src/main/java/decemberdate/DecemberDate.java
+++ src/main/java/decemberdate/DecemberDate.java
@@ -20,7 +20,7 @@ public class DecemberDate {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
         int date = Integer.parseInt(dateText);
-        if (date <= 0) {
+        if (date <= 0 || date > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
         return date;
